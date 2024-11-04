769ec779849d85d543c7d3cb25a3b22601061968
Phil Wright <95368282+phil198@users.noreply.github.com>
2024-11-04 14:42:35 +0000

    Immutable roles ()
    Co-authored-by: Lasse Heemann <lasse.heemann@neo4j.com>

--- community/cypher/front-end/ast/src/main/scala/org/neo4j/cypher/internal/ast/AdministrationAction.scala
+++ community/cypher/front-end/ast/src/main/scala/org/neo4j/cypher/internal/ast/AdministrationAction.scala
@@ -198,7 +198,9 @@ case object AssignPrivilegeAction extends PrivilegeManagementAction("ASSIGN PRIV
 
 case object RemovePrivilegeAction extends PrivilegeManagementAction("REMOVE PRIVILEGE")
 
-sealed trait UnassignableAction
+sealed trait UnassignableAction {
+  def customErrorMessage: Option[String] = None
+}
 
 case object AssignImmutablePrivilegeAction extends PrivilegeManagementAction("ASSIGN IMMUTABLE PRIVILEGE")
     with UnassignableAction
