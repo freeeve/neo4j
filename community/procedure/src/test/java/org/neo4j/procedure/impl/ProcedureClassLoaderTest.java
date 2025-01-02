ac2a315bd504e7da716397c2b0be002dedf6eed2
Matthew Nesbit <matthew.nesbit@neo4j.com>
2025-01-02 09:20:17 +0000

    Reduce metaspace usage by preloading less user defined classes ()

--- community/procedure/src/test/java/org/neo4j/procedure/impl/ProcedureClassLoaderTest.java
+++ community/procedure/src/test/java/org/neo4j/procedure/impl/ProcedureClassLoaderTest.java
@@ -30,10 +30,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.zip.ZipException;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.neo4j.configuration.GraphDatabaseInternalSettings;
 import org.neo4j.internal.kernel.api.exceptions.ProcedureException;
 import org.neo4j.logging.NullLog;
 import org.neo4j.test.extension.Inject;
@@ -53,9 +53,21 @@ class ProcedureClassLoaderTest {
         archive = testDirectory.file("extension.jar");
     }
 
-    @ParameterizedTest(name = "procedureReloadEnabled {0}, withExtension {1}")
-    @CsvSource({"true, true", "true, false", "false, true", "false, false"})
-    void whenJarIsOnClasspath(boolean procedureReloadEnabled, boolean withExtension)
+    @ParameterizedTest(name = "procedureReloadEnabled {0}, withExtension {1}, preload {2}")
+    @CsvSource({
+        "true, true, ALL",
+        "true, false, ALL",
+        "false, true, ALL",
+        "false, false, ALL",
+        "true, true, ANNOTATED",
+        "true, false, ANNOTATED",
+        "false, true, ANNOTATED",
+        "false, false, ANNOTATED"
+    })
+    void whenJarIsOnClasspath(
+            boolean procedureReloadEnabled,
+            boolean withExtension,
+            GraphDatabaseInternalSettings.ProcedureClassPreloading preload)
             throws IOException, ProcedureException {
         // Given a JAR with an extension on the classpath
         var appClassloader = makeClassloaderWithClasspath(archive);
@@ -68,7 +80,7 @@ class ProcedureClassLoaderTest {
 
         // when
         var result = ProcedureClassLoader.setup(
-                appClassloader, List.of(archive), NullLog.getInstance(), procedureReloadEnabled);
+                appClassloader, List.of(archive), NullLog.getInstance(), procedureReloadEnabled, preload);
 
         // If the feature is enabled, and the JAR does not contain extensions, then  we want the classloader
         // to be the ProcedureClassLoader that makes procedures reloadable. In other cases, we expect that
@@ -83,8 +95,9 @@ class ProcedureClassLoaderTest {
     }
 
     @ParameterizedTest(name = "withExtension {0}")
-    @ValueSource(booleans = {true, false})
-    void whenJarIsNotOnClasspath(boolean withExtension) throws IOException, ProcedureException {
+    @CsvSource({"false, ALL", "true, ALL", "false, ANNOTATED", "true, ANNOTATED"})
+    void whenJarIsNotOnClasspath(boolean withExtension, GraphDatabaseInternalSettings.ProcedureClassPreloading preload)
+            throws IOException, ProcedureException {
         // Given a JAR with an empty classpath
         var appClassloader = makeClassloaderWithClasspath();
 
@@ -99,16 +112,18 @@ class ProcedureClassLoaderTest {
                 appClassloader,
                 List.of(archive),
                 NullLog.getInstance(),
-                true // This can only occur with reloadProceduresFromDisk = true
-                );
+                true, // This can only occur with reloadProceduresFromDisk = true,
+                preload);
 
         // then we expect that all classes use the procedure classloader
         assertClassloader(result.loadedClasses(), result.loader());
     }
 
-    @Test
-    void validateClassloaderUsesParallelCapableFromURL() throws ZipException, ProcedureException {
-        var result = ProcedureClassLoader.setup(List.of(), NullLog.getInstance(), true);
+    @ParameterizedTest
+    @EnumSource()
+    void validateClassloaderUsesParallelCapableFromURL(GraphDatabaseInternalSettings.ProcedureClassPreloading preload)
+            throws ZipException, ProcedureException {
+        var result = ProcedureClassLoader.setup(List.of(), NullLog.getInstance(), true, preload);
 
         // We want our classloader to look and feel like an URLClassLoader
         var loader = new URLClassLoader(new URL[] {});
