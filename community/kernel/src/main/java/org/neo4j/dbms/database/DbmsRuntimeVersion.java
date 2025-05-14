42c460baa79cc66a073f70c735ddb2785cf2e65f
Emma Holmberg Ohlsson <emma.holmberg.ohlsson@neo4j.com>
2025-05-14 15:28:14 +0100

    Don't list DbmsRuntimeVersion.values an extra time in setup

--- community/kernel/src/main/java/org/neo4j/dbms/database/DbmsRuntimeVersion.java
+++ community/kernel/src/main/java/org/neo4j/dbms/database/DbmsRuntimeVersion.java
@@ -99,14 +99,8 @@ public enum DbmsRuntimeVersion implements ComponentVersion, KernelVersionProvide
 
     // The latest version should be kept private to be able to override it from tests.
     // getLatestVersion should be used when the latest version is required.
-    private static final DbmsRuntimeVersion LATEST_DBMS_RUNTIME_COMPONENT_VERSION = makeLatestVersion();
-
-    private static DbmsRuntimeVersion makeLatestVersion() {
-        var vals = DbmsRuntimeVersion.values();
-
-        // Select the second last element (the last being GLORIOUS_FUTURE).
-        return vals[vals.length - 2];
-    }
+    // Select the second last element (the last being GLORIOUS_FUTURE).
+    private static final DbmsRuntimeVersion LATEST_DBMS_RUNTIME_COMPONENT_VERSION = VERSIONS.get(VERSIONS.size() - 2);
 
     public static DbmsRuntimeVersion getLatestVersion(Config config) {
         Integer version = config.get(GraphDatabaseInternalSettings.latest_runtime_version);
