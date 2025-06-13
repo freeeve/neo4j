4d997ddedc7099e8d496b8f13b49be3be0e9477b
Love Kristofer Leifland <love.leifland@neotechnology.com>
2025-06-13 15:01:07 +0200

    Add tests to make sure Cypher 5 is not modified

---
+++ community/cypher/front-end/parser/v5/parser/src/test/java/org/neo4j/cypher/internal/parser/v5/Cypher5WatchdogTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.cypher.internal.parser.v5;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HexFormat;
+import org.junit.jupiter.api.Test;
+
+public class Cypher5WatchdogTest {
+
+    @Test
+    void cypher5IsFrozen() throws IOException, NoSuchAlgorithmException {
+        assertThat(sha256(Paths.get("src/main/antlr4/org/neo4j/cypher/internal/parser/v5/Cypher5Lexer.g4")))
+                .as("Cypher 5 is frozen. If you need to make changes, get approval from #team-clg first.")
+                .isEqualTo("530adb6ad41ace5f368c19241f2631fac23ebbccf060dcc082146fc8f10c80f6");
+        assertThat(sha256(Paths.get("src/main/antlr4/org/neo4j/cypher/internal/parser/v5/Cypher5Parser.g4")))
+                .as("Cypher 5 is frozen. If you need to make changes, get approval from #team-clg first.")
+                .isEqualTo("462d325be3ffb636bf1f24d480b1446ef507def937d6a3d4db844d755576f100");
+    }
+
+    private String sha256(Path filePath) throws IOException, NoSuchAlgorithmException {
+        final var digest = MessageDigest.getInstance("SHA-256");
+        return HexFormat.of().formatHex(digest.digest(Files.readAllBytes(filePath)));
+    }
+}
