8c47bb653fc81099ba93df4a1f195e6b4491820a
Fábio Botelho <dev.fbotelho@gmail.com>
2025-08-08 10:08:42 +0100

    Ignore RangeIndexIndexProviderApprovalTest in SPD ()

--- community/community-it/index-it/src/test/java/org/neo4j/kernel/api/impl/index/RangeIndexIndexProviderApprovalTest.java
+++ community/community-it/index-it/src/test/java/org/neo4j/kernel/api/impl/index/RangeIndexIndexProviderApprovalTest.java
@@ -24,7 +24,15 @@ import org.neo4j.dbms.api.DatabaseManagementService;
 import org.neo4j.graphdb.schema.IndexType;
 import org.neo4j.kernel.api.index.IndexProviderApprovalTest;
 import org.neo4j.test.TestDatabaseManagementServiceBuilder;
+import org.neo4j.test.extension.SkipOnSpd;
 
+@SkipOnSpd(
+        reason =
+                "The test inserts typed empty arrays in the database using core API that can't be effectively matched using cypher."
+                        + "The same thing would happen in a regular (i.e., non spd) database if you used core api to set a property p=new int[]  and then later used cypher to match p = []."
+                        + "Still, SPD introduces a deviation from a regular database shown by this test because now the results from an index property are different from the non indexed property."
+                        + "This happens because in a regular database we never resort to using cypher whereas in the SPD case we need to use cypher to talk with the shards",
+        notes = SkipOnSpd.Note.notSupported)
 public class RangeIndexIndexProviderApprovalTest extends IndexProviderApprovalTest {
     @BeforeAll
     public static void init() {
