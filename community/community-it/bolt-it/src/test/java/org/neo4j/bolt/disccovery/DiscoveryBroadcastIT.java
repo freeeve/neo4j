b9cba04d2f152c6f100600d382740eb7d2d17d45
Yuki <dotstart@pm.me>
2026-04-17 10:38:24 +0200

    Added some minimal tests for discovery beacons ()

---
+++ community/community-it/bolt-it/src/test/java/org/neo4j/bolt/disccovery/DiscoveryBroadcastIT.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.neo4j.bolt.disccovery;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.regex.Pattern;
+import org.assertj.core.api.Assertions;
+import org.neo4j.bolt.protocol.common.connector.transport.NioConnectorTransport;
+import org.neo4j.bolt.test.annotation.BoltTestExtension;
+import org.neo4j.bolt.test.annotation.connection.transport.IncludeTransport;
+import org.neo4j.bolt.test.annotation.setup.SettingsFunction;
+import org.neo4j.bolt.test.annotation.test.TransportTest;
+import org.neo4j.bolt.testing.assertions.discovery.BeaconSignalAssertions;
+import org.neo4j.bolt.testing.assertions.discovery.DiscoveryAssertions;
+import org.neo4j.bolt.testing.client.TransportType;
+import org.neo4j.bolt.testing.client.discovery.DiscoveryTestClient;
+import org.neo4j.bolt.transport.Neo4jWithSocketExtension;
+import org.neo4j.configuration.connectors.BoltConnector;
+import org.neo4j.configuration.connectors.BoltConnectorInternalSettings;
+import org.neo4j.configuration.helpers.SocketAddress;
+import org.neo4j.graphdb.config.Setting;
+import org.neo4j.test.extension.testdirectory.EphemeralTestDirectoryExtension;
+
+@EphemeralTestDirectoryExtension
+@Neo4jWithSocketExtension
+@BoltTestExtension
+public class DiscoveryBroadcastIT {
+
+    private static final Pattern DBMS_ID_PATTERN = Pattern.compile("^[A-Fa-f0-9]+$");
+    private static final Pattern NODE_ID_PATTERN =
+            Pattern.compile("^([A-Fa-f0-9]{8})-([A-Fa-f0-9]{4})-([A-Fa-f0-9]{4})-([A-Fa-f0-9]{4})-([A-Fa-f0-9]{12})$");
+
+    @SettingsFunction
+    static void customizeSettings(Map<Setting<?>, Object> settings) {
+        settings.put(BoltConnectorInternalSettings.enable_discovery, true);
+        settings.put(BoltConnectorInternalSettings.discovery_broadcast_interval, Duration.ofSeconds(5));
+        settings.put(BoltConnectorInternalSettings.discovery_broadcast_jitter, 0);
+        settings.put(BoltConnector.advertised_address, new SocketAddress("neo.example.org", 7687));
+    }
+
+    // FIXME: We're abusing the test extension a little here - Accommodate this
+    @TransportTest
+    @IncludeTransport(value = TransportType.TCP)
+    void shouldBroadcast() throws InterruptedException {
+        var client = new DiscoveryTestClient(new NioConnectorTransport());
+
+        DiscoveryAssertions.assertThat(client)
+                .receivesBeaconSignal(
+                        Duration.ofSeconds(30),
+                        BeaconSignalAssertions.create()
+                                // expect an average signal period of 5 seconds with a maximum deviation of +- 2 seconds
+                                .hasAverageSignalPeriod(Duration.ofSeconds(5), Duration.ofSeconds(2))
+                                .hasMagicNumber(0xDEADB017)
+                                .hasVarInt(0x01)
+                                .hasOpcode(0x01)
+                                .hasString((dbmsId) -> Assertions.assertThat(dbmsId)
+                                        .isNotEmpty()
+                                        .matches(DBMS_ID_PATTERN))
+                                .hasString((nodeId) -> Assertions.assertThat(nodeId)
+                                        .isNotEmpty()
+                                        .matches(NODE_ID_PATTERN))
+                                .hasString((productVersion) -> {
+                                    Assertions.assertThat(productVersion)
+                                            .satisfiesAnyOf(
+                                                    (version) -> Assertions.assertThat(version)
+                                                            // make sure we get a CalVer in the format YYYY.MM.PP while
+                                                            // disregarding any potential suffixes such as build numbers
+                                                            // or release stage
+                                                            .matches("^\\d{4,}\\.\\d{2}\\.\\d+.*"),
+                                                    (version) -> Assertions.assertThat(version)
+                                                            .isNotEmpty()
+                                                            // if running from within an IDE the version information is
+                                                            // typically unset
+                                                            .isEqualTo("dev"));
+                                })
+                                .hasString((edition) -> Assertions.assertThat(edition)
+                                        .isNotEmpty()
+                                        .isEqualTo("community"))
+                                .hasString((advertisementAddress) -> Assertions.assertThat(advertisementAddress)
+                                        .isNotEmpty()
+                                        .isEqualTo("neo.example.org:7687")));
+    }
+}
